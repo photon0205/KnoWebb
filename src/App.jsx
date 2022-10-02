885b58cd8c80d40128fc54f0e138220564bc50a6
nik-55 <nik.xyz.in@gmail.com>
2022-10-02 13:10:46 +0530

    Jumble Word Game Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Quiz from "./pages/quiz/Quiz";
 import Puzzle from "./pages/puzzle/Puzzle";
+import JumbleWord from "./pages/jumbleWord/JumbleWord"
+import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 
@@ -8,8 +10,10 @@ function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/puzzle" element={<Puzzle />} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/puzzle" element={<Puzzle />} />
+        <Route path={"/jumbleword"} element={<JumbleWord />} />
       </Routes>
     </Router>
   );
