38a938669f1fcd15daa6c9117e489b947681f246
nik-55 <nik.xyz.in@gmail.com>
2022-10-03 00:16:28 +0530

    minor updates

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -35,39 +35,33 @@ const Home = () => {
     const steps = [
         {
             label: 'Quiz',
-            description: `For each ad campaign that you create, you can control how much
-                    you're willing to spend on clicks and conversions, which networks
-                    and geographical locations you want your ads to show on, and more.`,
+            description: `Answer all the question correctly and get 
+            the MIRI instrument, one of the most important instrument of telescope.
+            It is used to detect Dust clouds, and planets etc...
+             It operates in the long IR Spectrum i.e. the wavelenghth that 
+             this instrument detects is longer than what the others detect`,
             path: "/quiz"
         },
         {
             label: 'Puzzle',
             description:
-                'An ad group contains one or more ads which target a shared set of keywords.',
+                'Align the blocks so that it forms the perfect image! To get the NIRISS instrument ',
+                
             path: "/puzzle"
         },
         {
             label: 'Slider',
-            description: `Try out different ad text to see what brings in the most customers,
-                      and learn how to enhance your ads using features like ad extensions.
-                      If you run into any problems with your ads, find out how to tell if
-                      they're running and how to resolve approval issues.`,
+            description: `Stop the slider by the spacebar at the perfect moment when the image is the most clear to get the NIRCam instrument! `,
             path: "/slider"
         },
         {
             label: 'Jumble Word',
-            description: `Try out different ad text to see what brings in the most customers,
-                    and learn how to enhance your ads using features like ad extensions.
-                    If you run into any problems with your ads, find out how to tell if
-                    they're running and how to resolve approval issues.`,
+            description: `Rearrange the letters in the lowest amount of time to get the perfect NIRSpec instrument!`,
             path: "/jumbleword"
         },
         {
             label: 'Order',
-            description: `Try out different ad text to see what brings in the most customers,
-                      and learn how to enhance your ads using features like ad extensions.
-                      If you run into any problems with your ads, find out how to tell if
-                      they're running and how to resolve approval issues.`,
+            description: `Put the optical elements of the telescope in the correct order for the perfect FGS instrument!`,
             path: "/order"
         },
     ];
