c5b0f821d477465d6f938d2f2eda30d2cd4f8649
nik-55 <nik.xyz.in@gmail.com>
2022-10-02 23:08:32 +0530

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Home from "./pages/home/Home"
 import Jumbleword from "./pages/jumbleWord/JumbleWord"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
-
+import Result from "./pages/result/result"
+import Order from "./pages/order/order.jsx"
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path={"/"} element={<Home/>}/>
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/puzzle" element={<Puzzle />} />
-          <Route path="/slider" element={<Slider />} />
-          <Route path={"/jumbleword"} element={<Jumbleword/>}/>
+        <Route path={"/"} element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/puzzle" element={<Puzzle />} />
+        <Route path="/slider" element={<Slider />} />
+        <Route path={"/jumbleword"} element={<Jumbleword />} />
+        <Route path={"/order"} element={<Order />} />
+        <Route path={"/result"} element={<Result />} />
       </Routes>
     </Router>
   );
